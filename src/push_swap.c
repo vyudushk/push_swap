70b6a7b68dcebe72124da121a14ad07bd2f35f49
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2017-07-17 12:11:47 -0700

    finnaly back to work, can handle input as a single string

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,13 +6,48 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/04 19:43:29 by vyudushk          #+#    #+#             */
-/*   Updated: 2017/07/08 03:01:44 by vyudushk         ###   ########.fr       */
+/*   Updated: 2017/07/17 12:11:06 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "libpush.h"
 #include <stdlib.h>
+
+int		isnumeric_str(char *str)
+{
+	if (*str == '-')
+		str++;
+	while (*str)
+	{
+		if ((*str > '9' || *str < '0') && *str != ' ')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+t_list	*str_to_lst(char *str)
+{
+	int		tmp;
+	t_list *lst;
+
+	tmp = 0;
+	lst = 0;
+	while (*str)
+	{
+		tmp = ft_atoi(str);
+		if (isnumeric_str(str) == 0 || isduplicate(lst, tmp) ||
+			ft_atol(str) > 2147483647 || ft_atol(str) < -2147483648)
+			leave();
+		ft_lstaddend(&lst, ft_lstnew(&tmp, sizeof(int)));
+		while (*str >= '0' && *str <= '9')
+			str++;
+		while (*str == ' ')
+			str++;
+	}
+	return (lst);
+}
 
 t_list	*arg_to_lst(int argc, char **argv)
 {
@@ -23,6 +58,8 @@ t_list	*arg_to_lst(int argc, char **argv)
 	i = 1;
 	if (argc == 1)
 		exit(0);
+	if (argc == 2)
+		return (str_to_lst(argv[1]));
 	tmp = 0;
 	lst = 0;
 	while (i < argc)
@@ -54,33 +91,6 @@ int		is_sort(t_list *lst)
 	return (1);
 }
 
-int		ft_lstlargest(t_list *lst)
-{
-	int	i;
-	int	*hold;
-
-	i = 0;
-	while (lst)
-	{
-		hold = lst->content;
-		if (i > *hold)
-			i = *hold;
-		lst = lst->next;
-	}
-	return (i);
-}
-
-void	sort(t_list **lst)
-{
-	t_list	*astk;
-	t_list	*bstk;
-
-	astk = *lst;
-	while (is_sort(lst) == 0)
-	{
-	}
-}
-
 int	main(int argc, char **argv)
 {
 	t_list	*lst;
@@ -89,5 +99,5 @@ int	main(int argc, char **argv)
 	if (is_sort(lst))
 		return (0);
 	//do the sorting
-	sort(&lst);
+	ft_putstr("do sort\n");
 }
